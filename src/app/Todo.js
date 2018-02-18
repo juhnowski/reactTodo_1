00c4a82b93942c3b76b8f33f0fd2974424844437
juhnowski <juhnowski@gmail.com>
2018-02-18 05:02:14 +0300

    first commit

---
+++ src/app/Todo.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: ["123","456"],
+      newTodo: ""
+    }
+  }
+
+  handleChange(e){
+    const {value} = e.target;
+    this.setState({newTodo: value});
+  }
+
+handleClick(e){
+  e.preventDefault();
+  const todos = [...this.state.todos, this.state.newTodo];
+  this.setState({todos, newTodo:''});
+}
+  render() {
+    return (
+      <div>
+        <form>
+          <input onChange={this.handleChange.bind(this)} value={this.state.newTodo} type="text" placeholder="new todo"/>
+          <button onClick={this.handleClick.bind(this)}>create</button>
+        </form>
+        <ul>
+          {this.state.todos.map(todo => <li key={todo}>{todo}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
